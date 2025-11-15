f803058c9a9704ee10af4616bfd5e0be7395be2d
miguel-jaeger <miguel.jaeger@gmail.com>
2025-11-14 19:41:06 -0500

    cmbio de excepcion

--- app/src/main/java/com/saludo/app/controlador/Saludo.java
+++ app/src/main/java/com/saludo/app/controlador/Saludo.java
@@ -7,18 +7,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Saludo {
     @GetMapping("/api/saludo/{nombre}")
-    public String saludo(@PathVariable String nombre) {
+    public ResponseEntity<String> saludo(@PathVariable String nombre) {
         if (nombre.length() >= 3) {
-            return "¡Bienvenido usuario: " + nombre + "!";
+            return ResponseEntity.ok("¡Bienvenido usuario: " + nombre + "!");
         } else {
-            throw new IllegalArgumentException("El nombre debe tener al menos 3 caracteres.");
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Error: El nombre debe tener al menos 3 caracteres.");
         }
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> manejarError(IllegalArgumentException ex) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body("Error: " + ex.getMessage());
     }
 }
